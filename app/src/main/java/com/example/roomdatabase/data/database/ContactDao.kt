26a1324e9ab45112312e8a65bb80bdfa3a053b86
Om kharade <kharadeoms@gmail.com>
2025-01-15 23:38:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/roomdatabase/data/database/ContactDao.kt
@@ -0,0 +1,25 @@
+package com.example.roomdatabase.data.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.roomdatabase.data.entity.Contact
+
+@Dao
+interface ContactDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertContact(contact: Contact)
+
+    @Delete
+    fun deleteContact(contact: Contact)
+
+    @Update
+    fun updateContact(contact: Contact)
+
+    @Query("SELECT * FROM Contact")
+    fun getAllContacts(): List<Contact>
+}
---
+++ app/src/main/java/com/example/roomdatabase/data/database/ContactDatabase.kt
@@ -0,0 +1,12 @@
+package com.example.roomdatabase.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.roomdatabase.data.entity.Contact
+
+@Database(entities = [Contact::class], version = 1, exportSchema = false)
+abstract class ContactDatabase: RoomDatabase() { // RoomDatabase is an abstract class that we need to extend by an abstract class
+    abstract var contactDao : ContactDao
+
+
+}
